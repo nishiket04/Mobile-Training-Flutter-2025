95d569a56214fdc14004a410124ae3bca820c875
Nishiket Bhatt <nishiket04@gmail.com>
2025-03-21 17:23:13 +0530

    Refectored

--- lib/images_fonts/images_fonts_task.dart
+++ lib/images_fonts/images_fonts_task.dart
@@ -11,13 +11,14 @@ class _ImageFonts extends State<ImageFonts>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: null,
       body: Stack(
+        fit: StackFit.expand,
         children: [
           Image.asset("assets/images/anime.jpg",fit: BoxFit.fill,),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: Text("Hello",style: TextStyle(fontFamily: "assets/fonts/Tektur-Medium.ttf",color: Colors.black,fontSize: 50.0),textAlign: TextAlign.center,),
+            alignment: Alignment.topCenter,
+            child: Text("Hello",style: TextStyle(fontFamily: "assets/fonts/Tektur-Medium.ttf",color: Colors.white,fontSize: 50.0),textAlign: TextAlign.center,),
           )
         ],
       ),
