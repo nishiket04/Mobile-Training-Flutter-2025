0166e4562c7756f60b372dfefe86792a918ec7b5
Nishiket Bhatt <nishiket04@gmail.com>
2025-03-21 15:05:41 +0530

    init

---
+++ android/app/src/main/kotlin/com/nishiket/flutter_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nishiket.flutter_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/images_fonts/images_fonts_task.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {"/imageFontsView": (context) => const ImageFonts()},
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/imageFontsView');
+              },
+              child: Text("Image And Fonts Task", textAlign: TextAlign.center,style: TextStyle(color: Colors.black87),),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.cyan),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
