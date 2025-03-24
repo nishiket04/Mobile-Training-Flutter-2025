7e45514c85fe6ed5616ba09de8c58ac558b905a3
Nishiket Bhatt <nishiket04@gmail.com>
2025-03-24 10:12:04 +0530

    minor refector

--- lib/images_fonts/images_fonts_task.dart
+++ lib/images_fonts/images_fonts_task.dart
@@ -11,18 +11,19 @@ class _ImageFonts extends State<ImageFonts>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset("assets/images/anime.jpg",fit: BoxFit.fill,),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text("Hello",style: TextStyle(fontFamily: "assets/fonts/Tektur-Medium.ttf",color: Colors.white,fontSize: 50.0),textAlign: TextAlign.center,),
-          )
-        ],
-      ),
-    );
+        body: SafeArea(
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset("assets/images/anime.jpg",fit: BoxFit.fill,),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text("Hello",style: TextStyle(fontFamily: "assets/fonts/Tektur-Medium.ttf",color: Colors.white,fontSize: 50.0),textAlign: TextAlign.center,),
+              )
+            ],
+          ),
+        ),
+      );
   }
 
 }
