7fe38ea5ec0e6d248df91ab547b483aa04f054cf
Nishiket Bhatt <nishiket04@gmail.com>
2025-04-11 15:16:00 +0530

    shared prefrence task

--- lib/task_json/seralization_task.dart
+++ lib/task_json/seralization_task.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_task/task_json/cart.dart';
 import 'package:flutter_task/task_json/cart_json.dart';
 import 'package:flutter_task/task_json/cart_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SeralizationTask extends StatefulWidget {
   const SeralizationTask({super.key});
@@ -13,12 +14,37 @@ class SeralizationTask extends StatefulWidget {
 }
 
 class _SeralizationTask extends State<SeralizationTask> {
-  static Iterable l = jsonDecode(CartJson.json);
-  List<Cart> cart = List<Cart>.from(l.map((model) => Cart.fromJson(model)));
+  late String json;
+  late Iterable l;
+  late List<Cart> cart;
+
+  @override
+  void initState() {
+    super.initState();
+    CartJson.getJson().then((value) {
+      l = jsonDecode(value);
+      setState(() {
+        cart = List<Cart>.from(l.map((model) => Cart.fromJson(model)));
+      });
+    });
+  }
+
+  Future<void> _storeUpdatedJson(String _json) async {
+    var pref = await SharedPreferences.getInstance();
+    pref.setString("cart_data", _json);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [TextButton(onPressed: _buildJsonDialog, child: Icon(Icons.shopping_cart_outlined))],),
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            onPressed: _buildJsonDialog,
+            child: Icon(Icons.shopping_cart_outlined),
+          ),
+        ],
+      ),
       body: GridView.builder(
         padding: EdgeInsets.all(3),
         physics: BouncingScrollPhysics(),
@@ -34,16 +60,15 @@ class _SeralizationTask extends State<SeralizationTask> {
     );
   }
 
-  void _buildJsonDialog(){
+  void _buildJsonDialog() {
+    _storeUpdatedJson(jsonEncode(cart));
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           content: SingleChildScrollView(
             child: Text(
-              jsonEncode(
-                cart,
-              ).replaceAll("},", "},\n").replaceAll("{", "\n{"),
+              jsonEncode(cart).replaceAll("},", "},\n").replaceAll("{", "\n{"),
             ),
           ),
         );
